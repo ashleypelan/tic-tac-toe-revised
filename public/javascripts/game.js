51f8368be4999cb44cacfe79827348dbfb5a2896
Ashley Pelan <ashley.pelan@gmail.com>
2016-01-04 16:29:36 -0700

    initial commit

---
+++ public/javascripts/game.js
@@ -0,0 +1,103 @@
+var tileArray = document.getElementsByClassName('tile');
+var tileValue = 'X';
+var computerPlay = document.getElementById('computer');
+var friendPlay = document.getElementById('friend');
+
+
+var clearBoard = function () {
+  [].forEach.call(tileArray, function (e) {
+    e.innerHTML = '';
+  });
+};
+
+var checkWinner = function (tileArray, button) {
+  for (var i = 0; i < tileArray.length; i+=3) {
+    if (tileArray[i].innerHTML === tileArray[i+1].innerHTML && tileArray[i].innerHTML === tileArray[i+2].innerHTML && tileArray[i].innerHTML !== '') {
+      alert('WINNA WINNA', button);
+      tileValue = 'O';
+      clearBoard();
+    }
+    else if (button === computerPlay) {
+      //fix here?
+    }
+  }
+  for (var i = 0; i < 3; i++) {
+    if (tileArray[i].innerHTML === tileArray[i+3].innerHTML && tileArray[i].innerHTML === tileArray[i+6].innerHTML && tileArray[i].innerHTML !== '') {
+      alert('WINNA WINNA');
+      tileValue = 'O';
+      clearBoard();
+    }
+  }
+  for (var i = 0; i < 3; i+=2) {
+    if(tileArray[i].innerHTML === tileArray[4].innerHTML && tileArray[i].innerHTML === tileArray[8-i].innerHTML && tileArray[i].innerHTML !== '') {
+      alert('WINNA WINNA');
+      tileValue = 'O';
+      clearBoard();
+    }
+  }
+};
+
+var tieCounter = function (tileArray, button) {
+  var counter = 0;
+  [].forEach.call(tileArray, function (e) {
+    if (e.innerHTML !== '') {
+      counter++;
+    }
+  });
+  if (counter === 9) {
+    alert("CAT WINZ");
+    tileValue = 'O';
+    clearBoard();
+  }
+};
+
+friendPlay.addEventListener('click', function () {
+  friendPlay.className = 'selected';
+  computerPlay.classList.remove('selected');
+  clearBoard();
+  tileArray = [].slice.call(tileArray,0);
+  [].forEach.call(tileArray, function (e, i, a) {
+    e.addEventListener('click', function() {
+      if (e.innerHTML === ""){
+        this.innerHTML = tileValue;
+        checkWinner(a, friendPlay);
+        tieCounter(a, friendPlay);
+        tileValue = tileValue === 'O' ? 'X' : 'O';
+      } else {
+        // alert('This tile has already been filled.');
+      }
+    });
+  });
+});
+
+computerPlay.addEventListener('click', function () {
+  computerPlay.className = 'selected';
+  friendPlay.classList.remove('selected');
+  clearBoard();
+  tileArray = [].slice.call(tileArray,0);
+  var computerMoveArray = [].map.call(tileArray, function (e, i) {
+    return i;
+  });
+  [].forEach.call(tileArray, function (e, i, a) {
+    e.addEventListener('click', function() {
+      if (e.innerHTML === ''){
+        this.innerHTML = tileValue;
+        checkWinner(a, computerPlay);
+        tieCounter(a, computerPlay);
+        tileValue = tileValue === 'O' ? 'X' : 'O';
+        computerMoveArray.splice(computerMoveArray.indexOf(i), 1);
+        if (computerMoveArray.length !== 0) {
+          var computerMove = Math.floor(Math.random() * (computerMoveArray.length));
+          tileArray[computerMoveArray[computerMove]].innerHTML = tileValue;
+          tileValue = tileValue === 'O' ? 'X' : 'O';
+          computerMoveArray.splice(computerMove, 1);
+          checkWinner(a, computerPlay);
+          tieCounter(a, computerPlay);
+        }
+      } else {
+        // alert('This tile has already been filled.');
+      }
+    });
+  });
+
+});
